712473bb43ca0091e90c4403670a5bef54086fbe
humphreymabeya <humphreymabeya@gmail.com>
2019-03-28 14:38:31 +0300

    New updates

--- Session/logout.php
+++ Session/logout.php
@@ -1,12 +1,4 @@
 <?php
-
-    // ob_start();
-    // session_start();
-    // unset($_SESSION['rainbow_name']);
-    // unset($_SESSION['rainbow_uid']);
-    // unset($_SESSION['rainbow_username']);
-    // echo '<script type="text/javascript">window.location="login.php"; </script>';
-
 // Initialize the session
 session_start();
  
